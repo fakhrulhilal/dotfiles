d3a14e3924e513dc3f23254e7a9e0969e2ce7b74
Fakhrulhilal M <fakhrulhilal@gmail.com>
2025-03-24 11:39:20 +0700

    feat: add more setup
    - Sample aliasing on SSH config
    - Use `lsd` as replacement of default `ls` command
    - Add Orbstack integration

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,7 @@
-alias ll="ls -lh"
+alias ll="lsd -lh"
 alias tree="lsd --tree"
 alias cls=clear
 #alias omp="eval $(oh-my-posh init zsh --config ~/proyek/dotfiles/config/posh-theme.omp.json)"
 alias compare=delta --pager "less -RF"
+alias gitc='git -P diff | delta --pager "less -RF"'
+alias gitcs='git -P diff --staged | delta --pager "less -RF"'
