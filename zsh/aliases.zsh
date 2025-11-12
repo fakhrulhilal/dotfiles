b9cdeea8650f867ed07157c070e6ac3429a30ff1
Fakhrulhilal M <fakhrulhilal@gmail.com>
2025-11-12 15:58:51 +0700

    feat(shell): add more alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,3 +5,4 @@ alias cls=clear
 alias compare=delta --pager "less -RF"
 alias gitc='git -P diff | delta --pager "less -RF"'
 alias gitcs='git -P diff --staged | delta --pager "less -RF"'
+alias gitcl='git -P show $(git rev-parse HEAD) | delta'
