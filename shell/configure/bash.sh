fbda7391d144ee2a4c39c65a6642619b3bfb798c
Fakhrulhilal M <fakhrulhilal@gmail.com>
2026-06-16 11:02:55 +0700

    feat(shell): various improvements
    - More robust bootstrapper script
    - Move shell completion generator to respectitive installer
    - Fix dotnet completion (use dynamic version which is more stable)
    - Clear zsh completion upon bootstrap initialization
    - Add fnox secret manager

--- shell/configure/bash.sh
+++ shell/configure/bash.sh
@@ -15,3 +15,21 @@ export BASH_EXT="\$DOT_HOME/bash"
 EOF
 
 source "$rc_file"
+completion_dir="$HOME/.local/share/bash-completion/completions"
+mkdir -p "$completion_dir"
+mise completion bash > "$completion_dir/mise"
+cat > "$completion_dir/bash" << 'EOF'
+# bash parameter completion for the dotnet CLI
+
+function _dotnet_bash_complete()
+{
+  local cur="${COMP_WORDS[COMP_CWORD]}" IFS=$'\n' # On Windows you may need to use use IFS=$'\r\n'
+  local candidates
+
+  read -d '' -ra candidates < <(dotnet complete --position "${COMP_POINT}" "${COMP_LINE}" 2>/dev/null)
+
+  read -d '' -ra COMPREPLY < <(compgen -W "${candidates[*]:-}" -- "$cur")
+}
+
+complete -f -F _dotnet_bash_complete dotnet
+EOF
