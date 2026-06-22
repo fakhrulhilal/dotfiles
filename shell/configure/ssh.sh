af87c8b44a82c0040f318916cbecac90edff873d
Fakhrulhilal M <fakhrulhilal@gmail.com>
2026-06-22 11:29:28 +0700

    feat(bootstrap): make sure ssh keys available

--- shell/configure/ssh.sh
+++ shell/configure/ssh.sh
@@ -7,4 +7,14 @@ if ! grep -Fxq "$LINE" "$CONFIG"; then
     mv "$CONFIG.tmp" "$CONFIG"
 fi
 
+if [ ! -f "$HOME/.ssh/id_rsa" ]; then
+    echo "🔐 Generating SSH key pair using RSA encryption"
+    ssh-keygen -t rsa -b 4096 -f "$HOME/.ssh/id_rsa"
+fi
+
+if [ ! -f "$HOME/.ssh/id_ed25519" ]; then
+    echo "🔐 Generating SSH key pair using ED25519 encryption"
+    ssh-keygen -t ed25519 -b 4096 -f "$HOME/.ssh/id_ed25519"
+fi
+
 unset CONFIG LINE
