35aaa8bc1aeb74f2914f4df85268b3b5d0210a88
Fakhrulhilal M <fakhrulhilal@gmail.com>
2026-07-06 11:48:22 +0700

    feat(bootstrap): insert at beginning of line

--- shell/configure/git.sh
+++ shell/configure/git.sh
@@ -1,14 +1,17 @@
 CONFIG="$HOME/.gitconfig"
+LINE="path = $DOT_HOME/config/git.txt"
 [ ! -f "$CONFIG" ] && touch "$CONFIG"
-if ! grep -q "^\[include\]" "$CONFIG"; then
-  cat >> "$HOME/.gitconfig" <<EOF
-
+if ! grep -Fq "$LINE" "$CONFIG"; then
+    cat > "$CONFIG.tmp" << EOF
 [include]
 	path = $DOT_HOME/config/git.txt
 
 [core]
     excludesfile = $DOT_HOME/config/git-ignore.txt
+
 EOF
+    cat "$CONFIG" >> "$CONFIG.tmp"
+    mv "$CONFIG.tmp" "$CONFIG"
 fi
 
-unset CONFIG
+unset CONFIG LINE
