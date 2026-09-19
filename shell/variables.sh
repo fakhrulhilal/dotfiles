d664d16a3a71eb8e16f4794204f59eba0254c418
Fakhrulhilal M <fakhrulhilal@gmail.com>
2026-09-19 09:14:50 +0700

    feat(config): Common settings
    - ASP.net core env vars
    - Exclude AI generated local file

--- shell/variables.sh
+++ shell/variables.sh
@@ -1,2 +1,8 @@
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
+export ASPNETCORE_URLS="http://*:8080"
+export ASPNETCORE_ENVIRONMENT="Development"
+
 export OTEL_DENO=true
+export WAKATIME_HOME="$HOME/.wakatime"
+export DEV_DOMAIN="dev.local"
+export SAN="DNS:*.blob.azurite.$DEV_DOMAIN, DNS:*.queue.azurite.$DEV_DOMAIN, DNS:*.table.azurite.$DEV_DOMAIN, DNS:*.$DEV_DOMAIN, DNS:$DEV_DOMAIN, DNS:*.lab, DNS:*.local, DNS:localhost, IP:127.0.0.1"
